ec7c31be8312b8cccb010d86b086d66b898a2788
liujinge <liujinge.work@gmail.com>
2022-09-07 10:45:08 +0800

    feat: 创建特性库

--- nest-org/apps/todos/src/app/app.module.ts
+++ nest-org/apps/todos/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosService } from './todos/todos.service';
+import { AuthModule } from '@nest-org/auth';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [AppController],
   providers: [AppService, TodosService],
 })
